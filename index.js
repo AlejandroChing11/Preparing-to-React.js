9aa1213937366a4312cd55198948c26e958aaa64
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-08 16:29:52 -0500

    Let's review some function basics

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hola Mundo ");
+function Hello() {
+    return 'Hola mundo';
+};
+
+ const result = Hello()
+
+
+function Alejo() {
+    return function () {
+        return 'Hola Mi nombre es Alejandro';
+    }
+};
+
+console.log(Alejo()()); 
+
+
+//Let's review parameters:
+
+function Sum(a, b) {
+    console.log(a + b);
+};
+
+Sum(2, 4);
