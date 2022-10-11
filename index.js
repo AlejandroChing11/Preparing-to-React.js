138099f0130a3c9dc0534b4d3366a8f839a08acf
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-11 11:57:20 -0500

    Learning DOM and how to use Javascript with Html

--- index.js
+++ index.js
@@ -1,33 +1,94 @@
-function Hello() {
-    return 'Hola mundo';
+// function Hello() {
+//     return 'Hola mundo';
+// };
+
+//  const result = Hello()
+
+
+// function Alejo() {
+//     return function () {
+//         return 'Hola Mi nombre es Alejandro';
+//     }
+// };
+
+// console.log(Alejo()()); 
+
+
+// //Let's review parameters:
+
+// function Sum(a, b) {
+//     console.log(a + b);
+// };
+
+// Sum(2, 4);
+
+// //Default Parameters:
+// function Mins(x, z = 0) {
+//     // if (z === undefined) {
+//     //     z = 0;
+//     // }
+//     return x - z;
+// };
+
+// console.log(Mins(2, 20));
+
+//Objetos 
+
+// const user = {
+//     name: 'ryan',
+//     lastname: 'castro',
+//     age: 27,
+//     address: {
+//         country: 'Colombia',
+//         city: 'Medellin',
+//         friends: ['Blessed', 'J balvin', 'nicky jam'],
+//     }
+// }
+
+// console.log(user.address);
+
+// console.log(user.age);
+
+// console.log(user.lastname);
+
+//shorthand and properties names.
+// const name = 'laptop';
+// const price = 27000;
+
+// const newProduct = {
+//     name, //We can name properties like that bc we have the same name of the property in a variable that is called 'name' and 'price'.
+//     price,
+// };
+
+// console.log(newProduct);
+
+//DOM manipulation. (Document Object Model)
+// const title = document.createElement("h1");
+
+// title.innerText = 'Hola mundo desde Js';
+
+// const button = document.createElement('button');
+// button.innerText = 'haz click aquí';
+
+// button.addEventListener("click", function (){
+//     console.log("Hola mundo");
+//     title.innerText = 'Texto actualizado con Js';
+//     alert('Se realizó un click');
+// })
+
+// document.body.append(button);
+// document.body.append(title);
+
+//Objects as an array.
+const user = {
+    name: 'Alejandro',
+    age: 18
 };
 
- const result = Hello()
+function printInfo(user) {
+    return '<h1> Hola ' + user.name + ' </h1>'
+}
 
+console.log(printInfo(user));
 
-function Alejo() {
-    return function () {
-        return 'Hola Mi nombre es Alejandro';
-    }
-};
-
-console.log(Alejo()()); 
-
-
-//Let's review parameters:
-
-function Sum(a, b) {
-    console.log(a + b);
-};
-
-Sum(2, 4);
-
-//Default Parameters:
-function Mins(x, z = 0) {
-    // if (z === undefined) {
-    //     z = 0;
-    // }
-    return x - z;
-};
-
-console.log(Mins(2, 20));
+document.body.innerHTML = printInfo(user);
